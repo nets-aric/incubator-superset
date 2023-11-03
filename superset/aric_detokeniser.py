e1b0fa5ef6b3f86bd9c41d26d84ced8cdb18cc3a
Veonms <robert@millersoftltd.com>
2023-11-03 14:51:06 +0000

    Changed approach to de-tokenisation functionality.

--- superset/aric_detokeniser.py
+++ superset/aric_detokeniser.py
@@ -39,32 +39,14 @@ session.headers.update({
 })
 
 
-def detokenise_json(df: DataFrame) -> DataFrame:
-    if df.dtype == 'object' and any(str(val).startswith('t:') if val is not None else False for val in df):
-        data = json.dumps({"id": df.to_list()})
-        req = session.post(config['DETOKENISE_POST_URL'],
-                           data=data)
-        return req.result().json()
-    return df
-
-
-async def to_thread(func, /, *args, **kwargs):
-    """Asynchronously run function *func* in a separate thread.
-    Any *args and **kwargs supplied for this function are directly passed
-    to *func*. Also, the current :class:`contextvars.Context` is propogated,
-    allowing context variables from the main thread to be accessed in the
-    separate thread.
-    Return a coroutine that can be awaited to get the eventual result of *func*.
-    """
-    loop = events.get_running_loop()
-    ctx = contextvars.copy_context()
-    func_call = functools.partial(ctx.run, func, *args, **kwargs)
-    return await loop.run_in_executor(None, func_call)
-
-
 async def detokenise_post_process(df: DataFrame) -> DataFrame:
-    data = await asyncio.gather(
-        *[to_thread(detokenise_json, df[col]) for col in df])
-    for count, col in enumerate(df):
-        df[col] = data[count]
+    filtered_tokens = {value for col_name in df.columns for value in df[col_name] if
+                       isinstance(value, str) and value.startswith('t:')}
+
+    detokenised_values = session.post(config['DETOKENISE_POST_URL'],
+                           data=json.dumps({"id": list(filtered_tokens)})).result()
+
+    result_dict = dict(zip(filtered_tokens, detokenised_values))
+    df.replace(result_dict, inplace=True)
+
     return df
